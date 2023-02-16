84818d44a6bcb4eeab28c10afd1726bab42dab45
choitim <choitim2010@gmail.com>
2023-02-15 22:04:26 -0600

    check box

--- cypress/integration/DEMOQA/Elements.spec.js
+++ cypress/integration/DEMOQA/Elements.spec.js
@@ -1,20 +1,19 @@
 ///<reference types="cypress" />
 
-describe("sdfg", () => {
-  it("form", () => {
+describe("Elements", () => {
+  beforeEach(() => {
     Cypress.on("uncaught:exception", (err, runnable) => {
       // returning false here prevents Cypress from
       // failing the test
       return false;
     });
-    cy.visit("https://demoqa.com/text-box");
-    // cy.get("#userName").type("test");
-    // cy.get("#userEmail").type("test@mail.ru");
-    // cy.get("#currentAddress").type("6906 S 28th W. Ave");
-    // cy.get("#permanentAddress").type("6906 S 28th W. Ave");
-    // cy.get("#submit").click();
-
-    cy.get('form[id="userForm"]').within(() => {
+    cy.visit("https://demoqa.com/elements");
+    cy.get ('div[class = "element-list collapse show"]').as ('list')
+  });
+  it("TextBox", () => {
+      cy.get ('@list').find('li[id = "item-0"]').click();
+      cy.url().should('contain', 'https://demoqa.com/text-box')
+      cy.get('form[id="userForm"]').within(() => {
       // scope формы
       cy.get("#userName").type("test");
       cy.get("#userEmail").type("test@mail.ru");
@@ -35,7 +34,11 @@ describe("sdfg", () => {
       cy.get ("#currentAddress").invoke('text').should('contain','6906 S 28th W. Ave');
       cy.get ("#permanentAddress").invoke('text').should('contain','kok301')
     });
-
-
   });
+  it.only ('Check Box', () => {
+      cy.get ('@list').find('li[id = "item-1"]').click();
+      cy.url().should('contain', 'https://demoqa.com/checkbox');
+      cy.get('label[for="tree-node-home"]').click()
+          //.should('be.checked')
+  })
 });
